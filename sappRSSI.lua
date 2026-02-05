e6f12d27fc3bb638f2ed676f9eb42963fbb44d0a
Jose Carmelo O. Almadrones <almadrones.jose.carmelo@gmail.com>
2026-02-05 13:30:31 +0800

    Add cyan/sapphire

---
+++ sappRSSI.lua
@@ -0,0 +1,76 @@
+-- RSSI LED Ring script
+-- This script is based on the throttle indicator on the LED bar
+-- by btastic_fpv
+
+local LED_GROUP1 = {10,11,12,13,14,15,16,17,18,19}
+local LED_GROUP2 = {0,1,2,3,4,5,6,7,8,9}
+
+local function init()
+end
+
+local function fillSlots(ledValue,ledGroup)
+	local numLedsTotal = #ledGroup
+	local segmentSize = 255 / numLedsTotal
+	
+  -- Calculate how many LEDs should be lit
+  local numLeds = math.floor(ledValue / segmentSize) + 1
+  numLeds = math.max(1, math.min(numLedsTotal, numLeds))
+  
+  -- Calculate brightness for the highest LED
+  local segment = (ledValue % segmentSize) / segmentSize
+  local brightness = math.floor(5 + (segment * 250))
+  
+  -- Special case: all LEDs full brightness
+  if ledValue == 255 then
+    numLeds = numLedsTotal
+    brightness = 255
+  end
+	
+	-- LEDs are sapphire
+  for i = 1, numLedsTotal, 1 do
+    local ledIndex = i - 1
+    if ledIndex < numLeds - 1 then
+      setRGBLedColor(ledGroup[i], 0, 255, 255)
+    elseif ledIndex == numLeds - 1 then
+      setRGBLedColor(ledGroup[i], 0, brightness, brightness)
+    else
+      setRGBLedColor(ledGroup[i], 0, 0, 0)
+    end
+  end 
+end
+
+local function calcLEDValue(source)
+
+	-- map -117dBm ... -3dBm into 0...255
+  local ledValue = math.floor((source + 117) * 255 / 114)
+	ledValue = math.max(0, math.min(255, ledValue))
+	
+	return ledValue
+end
+
+local function run()
+  -- Get RSSI Telem values
+  local elrs1RSS = getValue("1RSS")
+	local elrs2RSS = getValue("2RSS")
+	
+	local ledValue1 = calcLEDValue(elrs1RSS)
+	local ledValue2 = calcLEDValue(elrs2RSS)
+  
+  if not elrs1RSS == nil or elrs1RSS == 0 then
+		fillSlots(0,LED_GROUP1)
+	else
+		fillSlots(ledValue1,LED_GROUP1)
+	end
+	if not elrs2RSS == nil or elrs2RSS == 0 then
+		fillSlots(0,LED_GROUP2)
+	else
+		fillSlots(ledValue2,LED_GROUP2)
+	end
+  
+  applyRGBLedColors()
+end
+
+local function background()
+end
+
+return { run=run, background=background, init=init }
